24f9f86a0eac49467467734df0c730eaf8234589
DanexQ <danexq@gmail.com>
2023-06-07 23:37:13 +0200

    Update

--- components/Tasks/Tasks.tsx
+++ components/Tasks/Tasks.tsx
@@ -1,22 +1,28 @@
 "use client";
-import React, { use } from "react";
+import React from "react";
 import Task from "./Task";
 import PageNav from "./PageNav";
 import { useSearchParams } from "next/navigation";
 import useSWR from "swr";
+import { TasksDetailsType } from "@app/page";
+
+const fetcher = (url: string) => fetch(url).then((res) => res.json());
 
 const Tasks = () => {
   const searchParams = useSearchParams();
   const query = "?" + searchParams.toString();
-  const { data } = useSWR(`/api`);
-  console.log(data);
+  const { data, error } = useSWR<TasksDetailsType, Error>(
+    `/api${query}`,
+    fetcher
+  );
+  console.log(data?.tasks, data?.tasksQuantity);
   return (
     <section className="flex flex-col gap-5 text-gray-100 flex-[3_2_0%]">
-      {/* {data?.tasks.map((task) => (
+      {data?.tasks.map((task) => (
         <Task key={task.id} {...task} />
-      ))} */}
+      ))}
 
-      {/* <PageNav quantity={Math.ceil(data.tasksQuantity / 5)} /> */}
+      <PageNav quantity={data?.tasksQuantity || 1} />
     </section>
   );
 };
