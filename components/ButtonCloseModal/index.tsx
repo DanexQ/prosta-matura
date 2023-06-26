046d806e7f198e58ea51f07d264176a88d621857
Daniel <danexq@gmail.com>
2023-06-26 23:51:44 +0200

    Bug fixed

--- components/ButtonCloseModal/index.tsx
+++ components/ButtonCloseModal/index.tsx
@@ -4,12 +4,11 @@ import { useRouter } from "next/navigation";
 const ButtonCloseModal = ({ onClick }: { onClick?: () => void }) => {
   const router = useRouter();
 
-  const handleClick =
-    typeof onClick !== "undefined" ? onClick() : router.back();
+  const handleClick = typeof onClick !== "undefined" ? onClick : router.back;
 
   return (
     <button
-      onClick={() => handleClick}
+      onClick={() => handleClick()}
       className="absolute text-xl transition-all top-5 right-5 hover:text-fuchsia-500 active:scale-[0.95]"
     >
       &#10006;
